d50921c2f0fc35c641d4a28d720d97f1dea7bc5e
“1147403082@qq.com” <1147403082>
2022-04-02 16:09:59 +0800

    plus finished

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(ElementPlus).use(router).mount('#app')
